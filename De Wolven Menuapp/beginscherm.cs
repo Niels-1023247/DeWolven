67ac2077cc0a59226936d528615cf233ffb603f3
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2022-03-21 19:14:37 +0100

    Hoofdmenu alles, ALLES, van janell.

---
+++ De Wolven Menuapp/beginscherm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_Wolven_Menuapp
+{
+    internal class beginscherm
+    {
+        public void Begin()
+        {
+            Console.WriteLine("Welkom bij de wolven!");
+            Console.WriteLine("Bent u gast of medewerker?");
+            Console.WriteLine("[1] Gast");
+            Console.WriteLine("[2] Medewerker");
+            Console.WriteLine("Voer 1 of 2 in en druk op <ENTER>");
+        }
+    }
+}
