a1b8c664bf326661c0028c02dc001d9f9432044b
Niels van Klaveren <1023247@hr.nl>
2022-04-22 11:31:05 +0200

    sync

--- De Wolven Menuapp/Program.cs
+++ De Wolven Menuapp/Program.cs
@@ -13,10 +13,7 @@ namespace De_Wolven_Menuapp
     {
         public static void Main()
         {
-<<<<<<< HEAD
             //Loginfo.CreateAccount();
-=======
->>>>>>> c5b20f27cac13e6bd304b371addc2fa2c2efbe28
             Beginscherm.Begin();
         }
 
