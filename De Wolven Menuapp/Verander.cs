c318c915edf4bba312e8409b52ea3a033fabff1f
janellacharju <100201745+janellacharju@users.noreply.github.com>
2022-04-22 12:22:38 +0200

    veranderen reservering

--- De Wolven Menuapp/Verander.cs
+++ De Wolven Menuapp/Verander.cs
@@ -28,9 +28,30 @@ namespace De_Wolven_Menuapp
         public static void VeranderenReservering()
         {
             DisplayReserveringen();
+            var reserveringJson = File.ReadAllText("reserveringenbestand.json");
+            Information reserveringsData = JsonConvert.DeserializeObject<Information>(reserveringJson);
             Console.WriteLine("Voer de naam in van de reservering die je wilt veranderen: \n");
             string welkenaam = Console.ReadLine();
+            for (int i = 0; i < reserveringsData.Reserveringen.Count; i++) // ga elke reservering af
+            {
+                if (reserveringsData.Reserveringen[i].Name == welkenaam) // komt de nieuwe code overeen?
+                {
+                    Console.WriteLine("Voor wie is de reservering, voer de naam in:");
+                    string changeName = Console.ReadLine();
+                    reserveringsData.Reserveringen[i].Name = changeName;
+                    Console.WriteLine($"Voor welke datum is de reservering van {changeName}");
+                    string changeDate = Console.ReadLine();
+                    reserveringsData.Reserveringen[i].Date = changeDate;
+                    Console.WriteLine($"Voor welk tijdstip is de reservering?");
+                    string changeTime = Console.ReadLine();
+                    reserveringsData.Reserveringen[i].Time = changeTime;
+                    Console.WriteLine("Voor hoeveel mensen wilt u reserveren");
+                    string changeCountofPeople = Console.ReadLine();
+                    reserveringsData.Reserveringen[i].CountofPeople = changeCountofPeople;
 
+                }
+            }
+            
         }
     }  
 }
