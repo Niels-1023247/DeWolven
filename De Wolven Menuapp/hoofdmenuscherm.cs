6a5668a0bf4f286612a25897b83972c431d024f1
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2022-04-26 11:50:27 +0200

    merge error fixed

--- De Wolven Menuapp/hoofdmenuscherm.cs
+++ De Wolven Menuapp/hoofdmenuscherm.cs
@@ -13,16 +13,42 @@ namespace De_Wolven_Menuapp
         public static void SchermMedewerker()
         {
             Console.Clear();
-            Console.WriteLine("Welkom medewerker\n\n");
-            Console.WriteLine("Log in");
-            Console.WriteLine("Druk op Enter");
+            Console.WriteLine("Welkom alpha");
+            Console.WriteLine("[1] Bekijk de reserveringen");
+            Console.WriteLine("[2] Neem een bestelling op");
+            Console.WriteLine("[3] Maak een nieuwe reservering");
+            Console.WriteLine("[4] Bekijk besteltotaal per tafel");
+            Console.WriteLine("[5] Verander menukaart");
+            Console.WriteLine("Voer 1, 2, 3, 4 of 5");
 
-            ConsoleKey optieMedewerker = Console.ReadKey().Key;
-            if (optieMedewerker == ConsoleKey.Enter)
+
+            ConsoleKey optiemedewerker = Console.ReadKey().Key;
+            if (optiemedewerker == ConsoleKey.D1)
             {
-                Loginfo.Loginfoscherm("Medewerker");
+                Verander.DisplayReserveringen();
             }
+            else if (optiemedewerker == ConsoleKey.D2)
+            {
+                Console.Clear();
+                Bestellingopnemen.Bestelling();
 
+            }
+            else if (optiemedewerker == ConsoleKey.D3)
+            {
+                Console.Clear();
+                Reservering.AddReservering();
+
+            }
+            else if (optiemedewerker == ConsoleKey.D4)
+            {
+                Console.Clear();
+                Console.WriteLine("ook nog niks");
+            }
+            else if (optiemedewerker == ConsoleKey.D5)
+            {
+                Console.Clear();
+                Verander.VeranderenReservering();
+            }
         }
 
         public static void SchermAdmin()
