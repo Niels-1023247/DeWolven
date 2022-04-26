0734317ca14cc320f88010c8d76f5fea3ec434d1
Niels van Klaveren <1023247@hr.nl>
2022-04-26 12:22:24 +0200

    gefixt wat kevin zei

--- De Wolven Menuapp/hoofdmenuscherm.cs
+++ De Wolven Menuapp/hoofdmenuscherm.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace De_Wolven_Menuapp
 {
+    // hoofdmenu's voor elke soort gebruiker
     internal static class Hoofdmenuscherm
     {
         public static void SchermMedewerker()
@@ -27,27 +28,52 @@ namespace De_Wolven_Menuapp
         public static void SchermAdmin()
         {
             Console.Clear();
-            Console.WriteLine("Welkom Admin.\n\n");
-            Console.WriteLine("Momenteel is deze admin pagina nog onder constructie.");
+            Console.WriteLine("Welkom admin bij De Wolven");
+            Console.WriteLine("[1] Contactinformatie aanpassen");
+            Console.WriteLine("[2] Reservering aanpassen");
+            Console.WriteLine("\nFunctionaliteit wordt nog uitgebreid...\n\n");
+            Console.WriteLine("Voer 1 of 2 in...");
+            Console.WriteLine("Druk op esc om terug te gaan");
 
+            ConsoleKey optieadmin = Console.ReadKey().Key;
+            if (optieadmin == ConsoleKey.D1)
+            {
+                Console.Clear();
+                Contact.ChangeInfoMenu();
+            }
+
+            else if (optieadmin == ConsoleKey.D2)
+            {
+                Console.Clear();
+                Verander.VeranderenReservering();
+            }
+            else if (optieadmin == ConsoleKey.Escape)
+            {
+                Console.Clear();
+                Beginscherm.Begin();
+            }
         }
 
         public static void SchermKlanten()
         {
+            bool inlogstatus = Program.LoginCheck();
             Console.Clear();
-            Console.WriteLine("Welkom bij de wolven gast!");
+            if (inlogstatus) Console.WriteLine("Welkom bij de wolven, " + Program.ActiefAccountValues("Name") + "!");
+            else Console.WriteLine("Welkom bij de wolven!");
             Console.WriteLine("[1] Reserveer een tafel");
             Console.WriteLine("[2] Bekijk de menukaart");
-            Console.WriteLine("[3] Log in");
+            if (!inlogstatus) Console.WriteLine("[3] Log in");
             Console.WriteLine("[4] Registreer");
-            Console.WriteLine("[5] Over ons");
+            Console.WriteLine("[5] Reservering aanpassen");
+            Console.WriteLine("[6] Over ons");
             Console.WriteLine("Voer 1, 2, 3, 4 of 5 in");
             Console.WriteLine("Druk op esc om terug te gaan");
 
             ConsoleKey optieklanten = Console.ReadKey().Key;
             if (optieklanten == ConsoleKey.D1)
             {
-                Console.WriteLine("Nog niks");
+                Console.Clear();
+                Reservering.AddReservering();
             }
 
             else if (optieklanten == ConsoleKey.D2)
@@ -55,7 +81,7 @@ namespace De_Wolven_Menuapp
                 Console.Clear();
                 Menu.Menukaart();
             }
-            else if (optieklanten == ConsoleKey.D3)
+            else if (optieklanten == ConsoleKey.D3 && !inlogstatus)
 
             {
                 Loginfo.Loginfoscherm("Klant");
@@ -70,13 +96,22 @@ namespace De_Wolven_Menuapp
             else if (optieklanten == ConsoleKey.D5)
             {
                 Console.Clear();
+                Verander.reserveringAanpassenKlant();
+
+            }
+            else if (optieklanten == ConsoleKey.D6)
+            {
+                Console.Clear();
                 Contact.Contactgegevens();
 
             }
             else if (optieklanten == ConsoleKey.Escape)
             {
+                Console.Clear();
+                Program.SetLoginValues(null, null, null, null, null, null, false);
                 Beginscherm.Begin();
             }
+            else SchermKlanten();
         }
     }
 }
