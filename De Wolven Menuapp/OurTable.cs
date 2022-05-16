925fc8b19741f60cbb58044f8263a116eef8311a
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2022-05-16 10:34:39 +0200

    basis geschreven voor beschikbaarheid tafels

--- De Wolven Menuapp/OurTable.cs
+++ De Wolven Menuapp/OurTable.cs
@@ -14,27 +14,34 @@ namespace De_Wolven_Menuapp
         {
             var JsonString = File.ReadAllText("OurTable.json");
             var DeserialisedResult = JsonConvert.DeserializeObject<InGebruik>(JsonString);
+            string CurrentDate = Resv.Date;
             Tafels newTables = new Tafels
             {
 
             };
             try
             {
-
+                var addedTable = DeserialisedResult;
+                addedTable.Tafel[Resv.Date].Add(Resv.Time,newTables);
                 var newdate = new InGebruik
                 {
-                    Tafel = new Dictionary<string, Dictionary<string, Tafels>>()
+                    
                 };
             }
             catch (System.NullReferenceException)
             {
+                var tafeltijd = new Dictionary<string, Tafels> { Resv.Time, newTables };
+                Tafel = new Dictionary<string, Dictionary<string, Tafels>>
+                {
+                    {"initial", new Dictionary<string, Tafels>{"Initial",new Tafels}
+                    { CurrentDate, new Dictionary<string, Tafels> { Resv.Time, newTables } },
 
-            }
+             
             if (DeserialisedResult.Tafel[Resv.Date] == null)
             {
                 var newdate = new InGebruik
                 {
-                    Tafel=new Dictionary<string, Dictionary<string, Tafels>>
+                    
                 };
             }
             else if (DeserialisedResult.Tafel[Resv.Date] != null)
